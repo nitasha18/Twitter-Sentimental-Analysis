dde2339d4f66ff4d42bedb58b4078f117821d07a
nitasha18 <nitashag18@gmail.com>
2019-06-27 00:45:11 +0530

    added GUI

--- Sentimental_analysis.py
+++ Sentimental_analysis.py
@@ -7,11 +7,16 @@ import matplotlib.pyplot as plt
 from textblob import TextBlob
 from datetime import datetime, timedelta
 from nltk.tokenize import WordPunctTokenizer
+from tkinter import Tk, Label, INSERT, END, Entry, Button, Toplevel
 
 ACC_TOKEN=''
 ACC_SECRET=''
 CONS_KEY=''
 CONS_SECRET=''
+pos = 0  # total positive tweets count
+neg = 0  # total negative tweets count
+neut = 0  # total neutral tweets count
+final_score=0
 
 #connects to Twitter API
 def authentication(acc_token,acc_secret,cons_key,cons_secret):
@@ -66,11 +71,13 @@ def get_sentiment_score(tweet):
 
 #analyzing tweets
 def analyze_tweets(keyword,total_tweets):
-    score=0
+    f_score=0
+    global final_score
     tweets=search_tweets(keyword,total_tweets)
     for tweet in tweets:
         c_tweet=clean_tweet(tweet.text.encode('utf-8'))
         score=get_sentiment_score(c_tweet)
+        f_score+=score
         if score>0.0:
             status= str(score)+' || POSITIVE'
         elif score<0.0:
@@ -79,16 +86,18 @@ def analyze_tweets(keyword,total_tweets):
             status= str(score)+' || NEUTRAL'
         print('Tweet: {}'.format(c_tweet))
         print('Overall analysis: {}'.format(status))
-    final_score=round((score/float(total_tweets)),2)
+    final_score=round((f_score/float(total_tweets)),2)
     return final_score
 
 def display_result(final_score):
-    if final_score > 0.0:
-        final_status = str(final_score) + ' | POSITIVE | ✅'
-    elif final_score < 0.0:
-        final_status = str(final_score) + ' | NEGATIVE | ❌'
-    elif final_score == 0.0:
-        final_status = str(final_score) + ' | NEUTRAL | 🔶'
+    global final_status
+    if max(pos,neg,neut)==pos:
+        f_status=' || POSITIVE ||'
+    elif max(pos,neg,neut)==neg:
+        f_status=' || NEGATIVE ||'
+    elif max(pos,neg,neut)==neut:
+        f_status=' || NEUTRAL ||'
+    final_status = str(final_score) + f_status
     return final_status
 
 def draw_graphs(labels, sizes):
@@ -106,19 +115,86 @@ def draw_graphs(labels, sizes):
     plt.title('Tweets')
     plt.show()
 
-keyword='AUSVSENG'
-pos = 0  # total positive tweets count
-neg = 0  # total negative tweets count
-neut = 0  # total neutral tweets count
-final_score=display_result(analyze_tweets(keyword,100))
-labels=['Positive','Negative','Neutral']
-sizes=[pos,neg,neut]
-draw_graph(labels,sizes)      #draw pie and bar graph
+def GUI_graph():
+    labels=['Positive','Negative','Neutral']
+    sizes=[pos,neg,neut]
+    draw_graphs(labels,sizes)      #draw pie and bar graph
 
-print('-----------------------------------------')
-print('->Positive: '+str(pos))
-print('->Negative: '+str(neg))
-print('->Neutral: '+str(neut))
-print('-----------------------------------------')
-print('->Final score: '+final_score)
-print('-----------------------------------------')
+#GUI for displaying main result
+def GUI_displaying_score():
+    root1=Tk()
+    welcome=Label(root1,text='\n')
+    welcome.pack()
+    text0=Label(root1,
+               text='Twitter Sentimental Analysis on \''+keyword+'\'',
+               font='Times 28')
+    text1=Label(root1,
+               text='Positive tweets: '+str(pos)+'\nNegative tweets: '+str(neg)+'\nNeutral tweets: '+str(neut)+'\nFinal score: '+str(final_score),
+               font='Times 21')
+
+    text0.pack()
+    text1.pack()
+    welcome0 = Label(root1, text='\n')
+    welcome0.pack()
+    root.mainloop()
+
+def display_curr(root,keyword,limit):
+    dis0_text=Label(root,text='Keyword Searched: \''+keyword+'\' , Tweets Limit: '+str(limit)+'\n',font='Times 24')
+    dis0_text.pack()
+    score_analysis = Button(root, text='Score Analysis', font=inp_font, command=GUI_displaying_score)
+    score_analysis.pack()
+    space1 = Label(root, text='\n')
+    space1.pack()
+    graph= Button(root,text='Graph Analysis',font=inp_font,command=GUI_graph)
+    graph.pack()
+    space2 = Label(root, text='\n')
+    space2.pack()
+    exit_button = Button(root, text='Exit Window',font=inp_font, command=root.destroy)
+    exit_button.pack()
+    root.mainloop()
+
+#GUI
+root=Tk()
+root.geometry("1000x700+210+130")
+inp_font=('Times',21)
+inp_text1=Label(root,text='Enter the keyword for analysis: ',font='Times 24').pack()
+entry1= Entry(root,font=inp_font)
+entry1.pack()
+entry1.insert(INSERT,'')
+
+inp_text2=Label(root,text='Enter the Tweets\'s Limit: ',font='Times 24').pack()
+entry2= Entry(root,font=inp_font)
+entry2.pack()
+entry2.insert(INSERT,' ')
+
+def print_content():
+    global keyword
+    global limit
+    global final_score
+    keyword=str(entry1.get()).strip()
+    limit=int(entry2.get())
+    entry1.delete(0,END)
+    entry2.delete(0,END)
+    final_score = display_result(analyze_tweets(keyword, limit))
+    space=Label(root,text='\n')
+    space.pack()
+    display_curr(root,keyword,limit)
+    print('-----------------------------------------')
+    print('->Positive: ' + str(pos))
+    print('->Negative: ' + str(neg))
+    print('->Neutral: ' + str(neut))
+    print('-----------------------------------------')
+    print('->Final score: ' + final_score)
+    print('-----------------------------------------')
+
+button=Button(root,text='Submit',font=inp_font,command=print_content)
+button.pack()
+root.mainloop()
+
+
+
+
+
+
+
+
